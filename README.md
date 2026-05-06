045b7ec5eaec2a5ee9bd27f54867af4b68601c00
Dominic Chin <dominicchinkh@gmail.com>
2026-05-06 14:13:59 +1000

    Update an acceptance test for PhpBrowser

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# tutorial_codeception
+# Codeception
 
 This is a tutorial for Codeception, a PHP testing framework. It provides examples and explanations on how to use Codeception for various types of testing, including unit tests, functional tests, and acceptance tests.
 
@@ -25,13 +25,72 @@ php vendor/bin/codecept build
 ```
 
 ## Writing Tests
-You can create different types of tests using Codeception. For example, to create a unit test, you can run:
+You can create different types of tests using Codeception. 
+
+### Acceptance Tests
+Acceptance tests are used to test the application from the user's perspective. They simulate user interactions with the application and verify that it behaves as expected.
+
+For example, to create an acceptance test, you can run:
+
+```bash
+vendor/bin/codecept generate:cest Acceptance ExampleCest
+```
+
+This will create a new test file in the `tests/acceptance` directory. You can then write your test cases in this file.
+
+With the first argument you can run all tests from one suite:
+
+```bash 
+php vendor/bin/codecept run Acceptance
+```
+
+You can provide a directory path as well. This will execute all Acceptance tests from the backend dir:
+
+```bash
+php vendor/bin/codecept run Acceptance tests/Acceptance/backend
+```
+
+To limit tests run to a single class, add a second argument. Provide a local path to the test class, from the suite directory:
+
+```bash
+php vendor/bin/codecept run Acceptance ExampleCest
+php vendor/bin/codecept run tests/Acceptance/ExampleCest.php
+``` 
+
+You can further filter which tests to run by appending a method name to the class, separated by a colon:
+
+```bash
+php vendor/bin/codecept run Acceptance ExampleCest:^exampleTest$
+```
+
+In order to get more detailed output when running acceptance tests, you can use the `--steps` option:
+
+```bash
+php vendor/bin/codecept run Acceptance --steps
+```
+
+### Functional Tests
+Functional tests are used to test the application as a whole, including the interaction between different components. They are similar to acceptance tests but are more focused on the internal workings of the application rather than the user interface.
+
+For example, to create a functional test, you can run:
+
+```bash
+vendor/bin/codecept generate:cest Functional ExampleCest
+```
+
+This will create a new test file in the `tests/functional` directory. You can then write your test cases in this file.
+
+### Unit Tests
+Unit tests are used to test individual components or functions of your application in isolation. They help ensure that each part of your code works correctly on its own.
+
+For example, to create a unit test, you can run:
 
 ```bash
 vendor/bin/codecept generate:test unit ExampleTest
 ```
 
 This will create a new test file in the `tests/unit` directory. You can then write your test cases in this file.
+
 Here is an example of a simple unit test:
 
 ```php
@@ -44,6 +103,7 @@ class ExampleTest extends \Codeception\Test\Unit
 }
 ```
 
+### Running Tests
 You can run your tests using the following command:
 
 ```bash
--- symfony/tests/Acceptance/SignupCest.php
+++ symfony/tests/Acceptance/SignupCest.php
@@ -11,17 +11,131 @@ final class SignupCest
     public function _before(AcceptanceTester $I): void
     {
         // Code here will be executed before each test function.
+        $I->amOnPage('/signup');
+        $I->seeLink('Login');
+
+        /*-----------------
+         * Action for URLs
+         */
+        $I->seeInCurrentUrl('/signup');
+        $I->seeCurrentUrlEquals('/signup');
+        $I->seeCurrentUrlMatches('~^/s(\w+)p~');
+        
+        $slug = $I->grabFromCurrentUrl('~^/(\w+)~');
+        printf(" Slug value is: '%s'\n", $slug);
+
+        /*-------------------------------------
+         * Actions for checking the page title
+         */
+        $I->seeInTitle('Sign Up');
+        $I->dontSeeInTitle('Register');
+
+        /*--------------------
+         * Action for cookies
+         */
+        $I->setCookie('auth', '123345');
+        
+        $cookie = $I->grabCookie('auth');
+        printf(" Cookie value is: '%s'\n", $cookie);
+
+        $I->seeCookie('auth');
     }
 
     // All `public` methods will be executed as tests.
-    public function tryToTest(AcceptanceTester $I): void
+    public function signInSuccessfully(AcceptanceTester $I): void
     {
-        // Write your test content here.
+        // We can use the label, input name or id to match the `username` field
 
-        $I->amOnPage('/signup');
+        // To match fields by their labels, you should write a `for` attribute in the label tag.
+
+        /*------------
+         * Forms
+         * 
+         * To fill in all of the fields at once and send the form without clicking a ‘Submit’ button, 
+         * we can use the `submitForm` method:
+         * 
+         *   $I->submitForm(
+         *       '#signup-form', 
+         *       [
+         *           'user' => [
+         *               'username' => 'dominic@example.com',
+         *               'password' => 'password123',
+         *               'gender' => 'male'
+         *           ]
+         *       ],
+         *       'signup' // button values
+         *   );
+         * 
+         * The submitForm is not emulating a user’s actions.
+        */
         $I->fillField('username', 'dominic@example.com');
-        $I->fillField('password', 'password123');
+        $I->seeInField('username', 'dominic@example.com');
+
+        $username = $I->grabValueFrom('#username');
+        printf(" Username value is: '%s'\n", $username);
+
+        // PasswordArgument: to fill in sensitive data (like passwords) and hide it in logs
+        $I->fillField('password', new \Codeception\Step\Argument\PasswordArgument('password123')); 
+
+        // $password = $I->grabValueFrom('#password');
+        // printf(" Password value is: '%s'\n", $password);
+
+        $I->selectOption('gender', 'male');
+
+        $I->checkOption('agree-terms');
+        $I->seeCheckboxIsChecked('#agree-terms');
+
+        /*------- 
+         * Click
+         * 
+         * Alternative ways to click the "Sign Up" button:
+         * 
+         * // CSS selector applied
+         *   $I->click('#signup');
+         * 
+         * // XPath
+         *   $I->click('//button[@id="signup"]');
+         * 
+         * // Using context as second argument
+         *   $I->click('Sign Up', '.container');
+         * 
+         * // By specifying locator type
+         *   $I->click(['id' => 'signup']);
+         * 
+         * // Locator helper method
+         *   $I->click('Sign Up' , \Codeception\Util\Locator::isID('signup'));
+         */
+        $I->amGoingTo('submit user form with valid values');
         $I->click('Sign Up');
+        $I->expect('the form is submitted');
+       
+        /*------------
+         * Assertions
+         * 
+         * check that 'Thank you for Signing Up!' is inside an element with 'thank-you-message' id.
+         *   $I->see('Thank you for Signing Up!', '#thank-you-message');
+         */
         $I->see('Thank you for Signing Up!');
+
+        // We check this message is *not* on the page.
+        $I->dontSee('Form is filled incorrectly');
+
+        // You can check that a specific HTML element exists (or doesn’t) on a page
+        $I->seeElement('.notice');
+        $I->dontSeeElement('.error');
+
+        /*------------------------
+         * Conditional assertions
+         * 
+         * Usually, as soon as any assertion fails, further assertions of this test will be skipped. 
+         * Sometimes you don’t want this - maybe you have a long-running test and you want it to run 
+         * to the end. In this case, you can use conditional assertions.
+         * 
+         * Each see method has a corresponding canSee method, and dontSee has a cantSee method.
+         * 
+         * Each failed assertion will be shown in the test results, but it won’t stop the test.
+         */
+        $I->canSeeElement('.notice');
+        $I->cantSeeElement('.error');
     }
 }
